2e29d2cd6333235a61a9e8a4ad7062ef212cbf42
Nagarjun Redla <nagarjunredla@gmail.com>
2018-10-20 05:10:36 +0000

    Navigation test done

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/home_robot_ws/src/World/my_world.world " &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/home_service_robot_ws/src/World/my_world.world " &
 sleep 5
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun gmapping gmapping_demo.launch" &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch gmapping gmapping_demo.launch" &
 sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
