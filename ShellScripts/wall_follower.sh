3e5a7ae80e7d26c28c27dba349d79916c247fa61
Nagarjun Redla <nagarjunredla@gmail.com>
2018-10-20 23:18:38 +0000

    Better nav map

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -2,6 +2,7 @@
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/home_service_robot_ws/src/World/my_world.world " &
 sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch gmapping gmapping_demo.launch" &
+# xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_navigation gmapping_demo.launch" &
 sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
